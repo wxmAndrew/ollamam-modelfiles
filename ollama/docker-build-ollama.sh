8c04be54e69d512a490a624df0157a762aec1b42
Tanvir <tanvir.ahmed.tonoy@skiff.com>
2024-01-24 20:26:17 +0600

    update version variable in docker-build-ollama.sh

--- ollama/docker-build-ollama.sh
+++ ollama/docker-build-ollama.sh
@@ -2,13 +2,14 @@
 
 set -eu
 
-export VERSION=$(git rev-parse --abbrev-ref HEAD 2>/dev/null || echo "unknown")-$(git rev-parse --short HEAD 2>/dev/null || echo "0.0.0")
+export VERSION=$(git describe --tags --first-parent --abbrev=7 --long --dirty --always | sed -e "s/^v//g" 2>/dev/null || echo "0.0.0")-$(git rev-parse --abbrev-ref HEAD 2>/dev/null || echo "unknown")
 export GOFLAGS="'-ldflags=-w -s \"-X=github.com/ollama/ollama/version.Version=$VERSION\" \"-X=github.com/ollama/ollama/server.mode=release\"'"
 
 # Remove the container first
 docker stop ollama
 docker rm ollama
 
+# Build
 docker build \
     --platform=linux/amd64 \
     --build-arg=VERSION \
@@ -16,4 +17,5 @@ docker build \
     -f Dockerfile \
     -t ollama .
 
+# Run
 docker run -d --gpus=all -v ollama:/root/.ollama -v /home/user/workspace/ollama:/mnt/ollama -p 11434:11434 -e OLLAMA_ORIGINS="*" --name ollama --restart=always ollama
